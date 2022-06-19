9230be90a5005997dc901eefd585a2052c4024a2
Robin Enhorn <enhorn.robin@gmail.com>
2022-06-19 12:21:37 +0200

    Add tvOS and watchOS platforms.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,9 @@ let package = Package(
     name: "OneLogger",
     platforms: [
         .macOS(.v11),
-        .iOS(.v10)
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v6)
     ],
     products: [
         .library(
